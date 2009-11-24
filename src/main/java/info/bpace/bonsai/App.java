a97e68f6157c7102cf954ddb8886f9b39680b1c4
Blaine Pace <blainepace@gmail.com>
2009-11-24 12:24:53 -0800

    Initial import of empty maven project.

---
+++ src/main/java/info/bpace/bonsai/App.java
@@ -0,0 +1,13 @@
+package info.bpace.bonsai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
