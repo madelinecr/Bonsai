45f62c0b58ea65272ffe67f2c11d8835bde75564
Blaine Pace <blainepace@gmail.com>
2009-12-10 02:16:13 -0800

    Added ZVTMEventHandler class.

---
+++ src/main/java/info/bpace/bonsai/ZVTMEventHandler.java
@@ -0,0 +1,40 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details:
+ * http://www.gnu.org/licenses/gpl.txt
+ */
+
+package info.bpace.bonsai;
+
+import fr.inria.zvtm.engine.*;
+import fr.inria.zvtm.glyphs.Glyph;
+import fr.inria.zvtm.lens.Lens;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author Blaine Pace <blainepace at gmail.com>
+ */
+public class ZVTMEventHandler extends DefaultEventHandler
+{
+    @Override
+    public void mouseMoved(ViewPanel v, int jpx, int jpy, MouseEvent e)
+    {
+        
+
+    }
+
+    @Override
+    public void enterGlyph(Glyph g)
+    {
+        
+    }
+
+}
